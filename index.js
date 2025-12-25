e04dcdfdd0392aba6a119cdeb8490a13e5e5a6fe
mar1n <cykcykacz@gmail.com>
2025-12-25 18:09:26 +0000

    equilateral triangle

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function setup() {
-    createCanvas(800, 600)
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background(0)
+  background(220);
+
+  let side = 200;
+
+  // Bottom-left point
+  let x1 = 100;
+  let y1 = 300;
+
+  // Bottom-right point
+  let x2 = x1 + side;
+  let y2 = y1;
+
+  // Top point (centered)
+  let x3 = x1 + side / 2;
+  let y3 = y1 - (side * sqrt(3)) / 2;
+
+  triangle(x1, y1, x2, y2, x3, y3);
 }
