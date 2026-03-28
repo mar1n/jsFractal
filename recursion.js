f566ee17f1b992b896bd3e4ce745b1628925ae83
mar1n <cykcykacz@gmail.com>
2026-03-28 18:38:30 +0000

    test loop in recurison2

--- recursion.js
+++ recursion.js
@@ -1,11 +1,14 @@
 var stage = 5;
-var triangles = []
+var triangles = [{side: 0}]
 function recursion(value, ) {
     console.log('value', value)
+    console.log('triangle side test', triangles[value])
+    let side = triangles[value].side
   if (value === stage) {
     return;
   }
   value++;
+  triangles.push({side: side + 1})
   return recursion(value);
 }
 
